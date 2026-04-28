9753c833ac63b4a2fbd73cd459bb16b4abcc7240
Anna Moughnie <anna@moughnie.com>
2026-04-28 11:46:20 -0700

    Added db files and sample env

---
+++ BACKEND/db/databaseAccessLayer.js
@@ -0,0 +1,64 @@
+const database = include("/databaseConnection");
+const bcrypt = require("bcrypt");
+const saltRounds = 12;
+
+// async function getAllUsers() {
+//   let sqlQuery = `
+// 		SELECT * FROM web_user;
+// 	`;
+
+//   try {
+//     const results = await database.query(sqlQuery);
+//     console.log(results[0]);
+//     return results[0];
+//   } catch (err) {
+//     console.log("Error selecting from user table");
+//     console.log(err);
+//     return null;
+//   }
+// }
+
+// async function addUser(postData) {
+//   let hashedPassword = await bcrypt.hash(postData.password, saltRounds);
+//   let sqlInsertUser = `
+// INSERT INTO web_user (first_name, last_name, email, password_hash)
+// VALUES (:first_name, :last_name, :email, :hashedPassword);
+// `;
+//   let params = {
+//     first_name: postData.first_name,
+//     last_name: postData.last_name,
+//     email: postData.email,
+//     hashedPassword: hashedPassword,
+//   };
+//   console.log(sqlInsertUser);
+//   try {
+//     const results = await database.query(sqlInsertUser, params);
+//     let insertedID = results[0].insertId;
+//     console.log("🙋Inserted new user with ID:");
+//     console.log(insertedID);
+//     return true;
+//   } catch (err) {
+//     console.log(err);
+//     return false;
+//   }
+// }
+
+// async function deleteUser(webUserId) {
+//   let sqlDeleteUser = `
+// DELETE FROM web_user
+// WHERE web_user_id = :userID
+// `;
+//   let params = {
+//     userID: webUserId,
+//   };
+//   console.log(sqlDeleteUser);
+//   try {
+//     await database.query(sqlDeleteUser, params);
+//     return true;
+//   } catch (err) {
+//     console.log(err);
+//     return false;
+//   }
+// }
+
+// module.exports = { getAllUsers, addUser, deleteUser };
