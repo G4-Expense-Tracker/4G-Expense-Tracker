2033c8b72b1c21f0d226b32efc37e10197972fc7
Ria Bhullar <riabhullar44@gmail.com>
2026-04-28 11:30:26 -0700

    fix module syntax

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -1,8 +1,9 @@
 // express server here
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
-require('dotenv').config();
+dotenv.config();
 
 const app = express();
 app.use(cors());
