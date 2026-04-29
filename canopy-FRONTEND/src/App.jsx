1db44b22ae803f9db455d11f0792482236704d1d
Tina Shabestari <tshabestari@my.bcit.ca>
2026-04-29 09:42:16 -0700

    DEV-15 update start page UI to MUI-format not regular jsx

--- canopy-FRONTEND/src/App.jsx
+++ canopy-FRONTEND/src/App.jsx
@@ -7,17 +7,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import MainPage from './components/appstartpage'
+import MainPage from './components/MainPage'
 
 function App() {
 
   return (
+<<<<<<< HEAD
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Onboarding />} />
         <Route path="/main" element={<MainPage />} />
       </Routes>
     </BrowserRouter>
+=======
+    
+>>>>>>> c267337 (DEV-15 update start page UI to MUI-format not regular jsx)
     <MainPage />
   )
 }
