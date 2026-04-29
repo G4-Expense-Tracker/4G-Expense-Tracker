d490e6740be5cc48710304e160de61fd361445d9
Tina Shabestari-BCIT Student <72364547+Tinabcit@users.noreply.github.com>
2026-04-29 13:29:59 -0700

    Added the Login Page with MUI framework (#11)

--- canopy-FRONTEND/src/App.jsx
+++ canopy-FRONTEND/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import MainPage from "./components/MainPage";
 import Onboarding from "./components/onboarding/Onboarding";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Onboarding />} />
-        <Route path="/main" element={<MainPage />} />
+        <Route path="/" element={<Onboarding />}/>
+        <Route path="/main" element={<MainPage />}/>
+        <Route path="/login" element={<LoginPage />}/>
       </Routes>
     </BrowserRouter>
   );
