d73f605b7a0654c0c330561ff771d3f3283fc7bd
ava <avadavid48@gmail.com>
2026-04-30 14:56:35 -0700

    back arrow and skip functionality added

--- canopy-FRONTEND/src/components/onboarding/Onboarding.jsx
+++ canopy-FRONTEND/src/components/onboarding/Onboarding.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import OB_Footer from "./OB_Footer";
 import OB_Header from "./OB_Header";
 import OB_Step from "./OB_Step"
+import OB_End from "./OB_End";
 
 //IMAGES
 const img1 = "/onboarding_imgs/OB_IMG_1.png"
@@ -13,32 +14,33 @@ const img4 = "/onboarding_imgs/OB_IMG_4.png"
 function Onboarding() {
   const [step, setStep] = useState(0);
   const totalSteps = 4;
+  const [skipped, setSkipped] = useState(false)
 
-//STEPS
+  //STEPS
   const steps = [
     {
-        title:"Track Your Expenses",
-        image: img1,
-        alt: "iphones displaying Canopy app",
-        message: "See where your money goes"
+      title: "Track Your Expenses",
+      image: img1,
+      alt: "iphones displaying Canopy app",
+      message: "See where your money goes"
     },
     {
-        title:"Set Your Budget",
-        image: img2,
-        alt: "iphones displaying Canopy app",
-        message: "Set a daily or monthly budget. Stay on Track"
+      title: "Set Your Budget",
+      image: img2,
+      alt: "iphones displaying Canopy app",
+      message: "Set a daily or monthly budget. Stay on Track"
     },
     {
-        title:"Reach Your Goal",
-        image: img3,
-        alt: "iphone and tablet displaying Canopy app",
-        message: "Set small goals and build better spending habits over time."
+      title: "Reach Your Goal",
+      image: img3,
+      alt: "iphone and tablet displaying Canopy app",
+      message: "Set small goals and build better spending habits over time."
     },
     {
-        title:"Grow Your Tree",
-        image: img4,
-        alt: "tree sapling",
-        message: "Complete weekly quests to help the tree grow and reveal a special tree!"
+      title: "Grow Your Tree",
+      image: img4,
+      alt: "tree sapling",
+      message: "Complete weekly quests to help the tree grow and reveal a special tree!"
     }
   ]
 
@@ -47,19 +49,34 @@ function Onboarding() {
   const next = () => {
     if (step < totalSteps - 1) {
       setStep((prev) => prev + 1);
-    } else {
-      console.log("Onboarding complete");
     }
   };
 
-  return (
-    <div style={{ height: "100vh", position: "relative" }}>
-      <OB_Header />
-      {/* spread operator */}
-      <OB_Step {...steps[step]}/>
-      <OB_Footer step={step} totalSteps={totalSteps} onNext={next} />
-    </div>
-  );
+  const handleSkip = () => {
+    setSkipped(true)
+  }
+
+  const backHandler = () => {
+    setStep((prev) => prev - 1)
+  }
+
+  if (skipped) {
+    return (
+      <OB_End />
+    )
+
+
+  } else {
+
+    return (
+      <div style={{ height: "100vh", position: "relative" }}>
+        <OB_Header onSkip={handleSkip} onBack={backHandler} />
+        {/* spread operator */}
+        <OB_Step {...steps[step]} />
+        <OB_Footer step={step} totalSteps={totalSteps} onNext={next} />
+      </div>
+    )
+  }
 }
 
 export default Onboarding;
