596ac865e0c5fef730593548e92b45b59397720c
Tina Shabestari-BCIT Student <72364547+Tinabcit@users.noreply.github.com>
2026-04-30 11:33:39 -0700

    DEV-17 Create account for users using mui-framework and make sure the pages are linked together. (#14)

--- canopy-FRONTEND/src/App.jsx
+++ canopy-FRONTEND/src/App.jsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import Onboarding from "./components/onboarding/Onboarding";
 import LoginPage from "./components/LoginPage";
+import SignupPage from "./components/SignupPage";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Onboarding />}/>
         <Route path="/main" element={<MainPage />}/>
         <Route path="/login" element={<LoginPage />}/>
+        <Route path="/signup" element={<SignupPage />}/>
+        <Route path="/onboarding" element={<Onboarding />}/>
       </Routes>
     </BrowserRouter>
   )
