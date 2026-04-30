7202aabff7c11aa89e76d66d5baa9d5478a71e91
ava <avadavid48@gmail.com>
2026-04-30 14:56:35 -0700

    back arrow and skip functionality added

--- canopy-FRONTEND/src/components/onboarding/OB_Header.jsx
+++ canopy-FRONTEND/src/components/onboarding/OB_Header.jsx
@@ -1,20 +1,20 @@
 import { Box, Typography } from "@mui/material";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
-function OB_Header() {
+function OB_Header({onSkip, onBack}) {
   return (
     <Box
       sx={{
         display: "flex",
-        justifyContent: "space-between", // 👈 spreads items
+        justifyContent: "space-between",
         alignItems: "center",
         pt: 6,
         px: 2,
       }}
     >
-      <ArrowBackIosIcon />
+      <ArrowBackIosIcon onClick={ onBack }/>
 
-      <Typography variant="body2">Skip</Typography>
+      <Typography variant="body2" onClick={onSkip}>Skip</Typography>
     </Box>
   );
 }
