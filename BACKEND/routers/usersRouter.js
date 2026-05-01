3a6f76e1d31b810ffb05ee57033dc8c3924515f6
Ria Bhullar <riabhullar44@gmail.com>
2026-05-01 10:46:10 -0700

    add user route functionality

--- BACKEND/routers/usersRouter.js
+++ BACKEND/routers/usersRouter.js
@@ -1,29 +1,89 @@
 import express from "express";
 const router = express.Router();
-import cors from "cors";
 
-router.get('/login', (req, res) => {
+import {
+  getUserById,
+  addUser,
+  verifyUserCredentials,
+  getUserByEmail
+} from "../db/dal/user.js";
 
+router.get('/login', async (req, res) => {
+    if (!req.session.userId) {
+        return res.json({ isLoggedIn: false })
+    }
+
+    const user = await getUserById(req.session.userId)
+
+    if (!user) {
+        req.session = null;
+        return res.json({ isLoggedIn: false })
+    }
+
+    res.json({
+        isLoggedIn: true,
+        user: {
+        id: user.user_id,
+        email: user.email,
+        first_name: user.first_name,
+        },
+    });
 })
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
+    const user =  await verifyUserCredentials(email, password)
+
+    if (!user) {
+        return res.status(400).json({ error: "Login failed" });
+    }
+
+    req.session.userId = user.user_id;
+
+    res.json({
+        isLoggedIn: true,
+        user: {
+        id: user.user_id,
+        email: user.email,
+        first_name: user.first_name,
+        },
+    });
 })
 
-router.get('/register', (req, res) => {
+router.post('/register', async (req, res) => {
+    const { first_name, last_name, email, password, phone_number } = req.body;
 
-})
+    if (!email || !password || !first_name) {
+        return res.status(400).json({ error: "Missing required fields" });
+    }
 
-router.post('/register', (req, res) => {
+    const existingUser = await getUserByEmail(email)
 
-})
+    if (existingUser) {
+        return res.status(400).json({ error: "User with that email already exists" });
+    }
 
-router.get('/logout', (req, res) => {
+    const addResult = await addUser({
+        first_name,
+        last_name,
+        email,
+        password,
+        phone_number,
+    });
 
+    if (!addResult.success) {
+        return res.status(500).json({ error: "Failed to register" });
+    }
+
+    req.session.userId = addResult.insertedID;
+
+    res.json({ message: "User registered" });
 })
 
 router.post('/logout', (req, res) => {
-
+    req.session = null;
+    res.json({ message: "Logged out" });
 })
 
 export default router;
