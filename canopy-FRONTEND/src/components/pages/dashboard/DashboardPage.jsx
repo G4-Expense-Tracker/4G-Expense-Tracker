aa3b330546b4af02d3acd237d5ca5d9a83a58d62
Tina Shabestari <tshabestari@my.bcit.ca>
2026-05-06 16:20:46 -0700

    test out the functionality

--- canopy-FRONTEND/src/components/pages/dashboard/DashboardPage.jsx
+++ canopy-FRONTEND/src/components/pages/dashboard/DashboardPage.jsx
@@ -7,7 +7,7 @@ import BatteryFullIcon from "@mui/icons-material/BatteryFull";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
-import Footer from "../dashboard/Footer";
+import Footer from "../../pages/dashboard/Footer";
 
 const dashboards = [
   { title: "Click here", subtitle: "to set your goal", emoji: "🌱", progress: 0 },
