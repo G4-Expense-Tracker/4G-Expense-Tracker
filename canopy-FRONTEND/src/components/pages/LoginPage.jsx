59d0539b1ce6579c9d76b6658087907268eb07f7
Tina Shabestari <tshabestari@my.bcit.ca>
2026-05-07 13:20:00 -0700

    trying to match the colour to the original design

--- canopy-FRONTEND/src/components/pages/LoginPage.jsx
+++ canopy-FRONTEND/src/components/pages/LoginPage.jsx
@@ -142,7 +142,7 @@ export default function LoginPage() {
             bgcolor: "primary.main",
             color: "#fff",
             mb: 2,
-            boxShadow: "0px 6px 10px rgb(255, 0, 0)",
+            boxShadow: "0px 6px 10px rgb(251, 250, 250)",
             "&:hover": { bgcolor: "primary.dark" },
           }}
         >
