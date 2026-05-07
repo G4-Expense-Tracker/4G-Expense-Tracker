1752e1ea685f1f68f23ebe58a0adbe0d7a4fcc28
Ria Bhullar <riabhullar44@gmail.com>
2026-05-07 09:33:24 -0700

    fix user routes with middleware

--- BACKEND/routers/usersRouter.js
+++ BACKEND/routers/usersRouter.js
@@ -3,75 +3,86 @@ const router = express.Router();
 
 import { requireLogin, requireLogout } from "../middleware/authMiddleware.js";
 import {
-  getUserById,
   addUser,
   verifyUserCredentials,
   getUserByEmail
 } from "../db/dal/user.js";
 
-router.get('/session',requireLogin ,async (req, res) => {
+router.get('/session', requireLogin , (req, res) => {
     res.json({
         isLoggedIn: true,
         user: {
-        id: user.user_id,
-        email: user.email,
-        first_name: user.first_name,
+        id: req.user.user_id,
+        email: req.user.email,
+        first_name: req.user.first_name,
         },
     });
 })
 
-router.post('/login',requireLogout , async (req, res) => {
-    const { email, password } = req.body;
+router.post('/login', requireLogout , async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-    const user =  await verifyUserCredentials(email, password)
+        const user =  await verifyUserCredentials(email, password)
 
-    if (!user) {
-        return res.status(400).json({ error: "Login failed" });
+        if (!user) {
+            return res.status(400).json({ error: "Login failed" });
+        }
+
+        req.session.userId = user.user_id;
+
+        res.json({
+            isLoggedIn: true,
+            user: {
+            id: user.user_id,
+            email: user.email,
+            first_name: user.first_name,
+            },
+        });
+    } catch(err) {
+        console.error(err);
+
+        res.status(500).json({ error: "Server error" });
     }
-
-    req.session.userId = user.user_id;
-
-    res.json({
-        isLoggedIn: true,
-        user: {
-        id: user.user_id,
-        email: user.email,
-        first_name: user.first_name,
-        },
-    });
 })
 
-router.post('/register',requireLogout ,async (req, res) => {
-    const { first_name, last_name, email, password, phone_number } = req.body;
+router.post('/register', requireLogout ,async (req, res) => {
+    try {
+        const { first_name, last_name, email, password, phone_number } = req.body;
 
-    if (!email || !password || !first_name) {
-        return res.status(400).json({ error: "Missing required fields" });
+        if (!email || !password || !first_name) {
+            return res.status(400).json({ error: "Missing required fields" });
+        }
+
+        const existingUser = await getUserByEmail(email)
+
+        if (existingUser && Object.keys(existingUser).length > 0) {
+            return res.status(400).json({ error: "User with that email already exists" });
+        }
+
+        const addResult = await addUser({
+            first_name,
+            last_name,
+            email,
+            password,
+            phone_number,
+        });
+
+        if (!addResult.success) {
+            return res.status(500).json({ error: "Failed to register" });
+        }
+
+        req.session.userId = addResult.insertedID;
+
+        res.json({ message: "User registered", isLoggedIn: true });
+    } catch(err) {
+        console.error(err);
+
+        res.status(500).json({ error: "Server error" });
     }
-
-    const existingUser = await getUserByEmail(email)
-
-    if (existingUser && Object.keys(existingUser).length > 0) {
-        return res.status(400).json({ error: "User with that email already exists" });
-    }
-
-    const addResult = await addUser({
-        first_name,
-        last_name,
-        email,
-        password,
-        phone_number,
-    });
-
-    if (!addResult.success) {
-        return res.status(500).json({ error: "Failed to register" });
-    }
-
-    req.session.userId = addResult.insertedID;
-
-    res.json({ message: "User registered", isLoggedIn: true });
 })
 
-router.post('/logout',requireLogin , (req, res) => {
+router.post('/logout', requireLogin , (req, res) => {
     req.session = null;
     res.json({ message: "Logged out", isLoggedIn: false });
 })
