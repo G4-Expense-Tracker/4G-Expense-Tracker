e0caea8af023d6a08f74f4e14f231505f3b0e5a5
Ria Bhullar <riabhullar44@gmail.com>
2026-05-07 09:24:51 -0700

    add db function to login auth

--- BACKEND/middleware/authMiddleware.js
+++ BACKEND/middleware/authMiddleware.js
@@ -1,12 +1,29 @@
-export function requireLogin(req, res, next) {
-  if (!req.session || !req.session.userId) {
-    return res.status(401).json({ error: "Please log in to access this feature" });
+import { getUserById } from "../db/dal/user";
+
+export async function requireLogin(req, res, next) {
+  try {
+    if (!req.session?.userId) {
+      return res.status(401).json({ error: 'Please log in to access this feature'})
+    }
+
+    const user = await getUserById(req.session.userId)
+
+    if (!user) {
+      req.session.userId = null;
+      return res.status(401).json({ error: "User no longer exists" });
+    }
+
+    req.user = user;
+
+    next();
+  } catch(err) {
+    console.error(err)
+    return res.status(500).json({error: 'Failed to '})
   }
-  next();
 }
 
 export function requireLogout(req, res, next) {
-  if (req.session && req.session.userId) {
+  if (req.session?.userId) {
     return res.status(403).json({ error: "Already logged in." });
   }
   next();
