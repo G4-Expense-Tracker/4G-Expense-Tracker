976849fda105aaa1d97f333d9c553d919f09b55d
Tina Shabestari <tshabestari@my.bcit.ca>
2026-05-07 17:01:04 -0700

    pull the changes from the dev branch

--- canopy-FRONTEND/src/App.jsx
+++ canopy-FRONTEND/src/App.jsx
@@ -11,6 +11,7 @@ import Goal from "./components/goal_pages/Goal";
 import DashboardPage from "./components/pages/dashboard/DashboardPage";
 import ExpenseDashboard from "./components/pages/dashboard/ExpenseDashboard";
 import CategoryPage from "./components/profile_pages/category/CategoryPage";
+import AddCategoryPage from "./components/profile_pages/category/AddCategoryPage";
 import NewGoal from "./components/goal_pages/NewGoal";
 import Accessibility from "./components/profile_pages/accessibility/Accessibility";
 
