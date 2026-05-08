b1d5ed85b42d41d1bb53b6216d2f66d47ba8e88f
Anna Moughnie <anna@moughnie.com>
2026-05-08 10:31:33 -0700

    Fixed addTree

--- BACKEND/db/dal/tree.js
+++ BACKEND/db/dal/tree.js
@@ -45,13 +45,14 @@ export async function getTreesByUser(user_id) {
 
 export async function addTree(postData) {
   const query = `
-    INSERT INTO tree (user_id, tree_type, date_earned)
-    VALUES (:user_id, :tree_type, CURDATE());
+    INSERT INTO tree (user_id, tree_type, goal_id, date_earned)
+    VALUES (:user_id, :tree_type, :goal_id, CURDATE());
     `;
 
-  // const params = {
-  //   user_id,
-  // };
+  //   postData MUST HAVE:
+  //     user_id
+  //     tree_type (INT 1 to 5)
+  //     goal_id
 
   try {
     const results = await database.query(query, postData);
