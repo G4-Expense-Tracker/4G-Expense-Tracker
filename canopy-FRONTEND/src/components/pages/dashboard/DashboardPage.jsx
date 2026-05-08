1677e6898f54f9fee5393d8f27f7dd48a26cc236
Tina Shabestari <tshabestari@my.bcit.ca>
2026-05-08 10:49:30 -0700

    DEV 76 Added the Footer component to its own folder

--- canopy-FRONTEND/src/components/pages/dashboard/DashboardPage.jsx
+++ canopy-FRONTEND/src/components/pages/dashboard/DashboardPage.jsx
@@ -7,7 +7,7 @@ import BatteryFullIcon from "@mui/icons-material/BatteryFull";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 
-import Footer from "./FooterNav";
+import FooterNav from "../../Footer/FooterNav";
 
 const dashboards = [
   { title: "Click here", subtitle: "to set your goal", emoji: "🌱", progress: 0 },
