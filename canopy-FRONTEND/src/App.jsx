3185b236a4e0d1386688e62baf9486686dc6db39
Tina Shabestari <tshabestari@my.bcit.ca>
2026-05-08 20:44:36 -0700

    Fixing the app.jsx file

--- canopy-FRONTEND/src/App.jsx
+++ canopy-FRONTEND/src/App.jsx
@@ -16,11 +16,8 @@ import AddCategoryPage from "./components/profile_pages/category/AddCategoryPage
 import NewGoal from "./components/goal_pages/NewGoal";
 import Accessibility from "./components/profile_pages/accessibility/Accessibility";
 import AccountPage from "./components/profile_pages/account/AccountPage";
-<<<<<<< HEAD
 import ProfilePage from "./components/profile_pages/profileandlogout/ProfilePage";
 import LogoutPage from "./components/profile_pages/profileandlogout/LogoutPage";
-=======
->>>>>>> 4e383f5a846c2221dd271bf3aed63e2512fd7fed
 import Language from "./components/profile_pages/Language";
 
 function App() {
@@ -41,11 +38,8 @@ function App() {
         <Route path="/add-category" element={<AddCategoryPage />} />
         <Route path="/accessibility" element={<Accessibility />} />
         <Route path="/account" element={<AccountPage />} />
-<<<<<<< HEAD
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="/logout" element={<LogoutPage />} />
-=======
->>>>>>> 4e383f5a846c2221dd271bf3aed63e2512fd7fed
         <Route path="/language" element={<Language />} />
 
 
