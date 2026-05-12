0e73d051db417c087b32573923a6b6d96bd9a685
Tina Shabestari <tshabestari@my.bcit.ca>
2026-05-12 12:08:32 -0700

    Created the Expense page and added import footer from Expense page

---
+++ canopy-FRONTEND/src/components/pages/expenses/Expense.jsx
@@ -0,0 +1,21 @@
+// React useState reference:
+// https://react.dev/reference/react/useState
+import { useState } from "react";
+
+// React Router useNavigate reference:
+// https://reactrouter.com/api/hooks/useNavigate
+import { useNavigate } from "react-router-dom";
+
+// MUI components reference:
+// https://mui.com/material-ui/getting-started/
+import {
+  Box,
+  Typography,
+  CircularProgress,
+  IconButton,
+  LinearProgress,
+  TextField,
+} from "@mui/material";
+
+/* Shared footer component */
+import FooterNav from "../../Footer/FooterNav";
