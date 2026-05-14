113fc53638c41acb88828b5080b77ac669353aa1
Ria Bhullar <riabhullar44@gmail.com>
2026-05-14 09:39:35 -0700

    add logout api call

--- canopy-FRONTEND/src/api/users.js
+++ canopy-FRONTEND/src/api/users.js
@@ -16,3 +16,37 @@ export async function loginUser(email, password) {
 
   return data;
 }
+
+export async function logoutUser() {
+    const res = await fetch(import.meta.env.VITE_APP_LOGOUT_URL, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error(data.error || 'Logout failed')
+    }
+
+    return data;
+}
+
+export async function registerUser() {
+
+}
+
+export async function getUserSession() {
+
+}
+
+export async function editUser() {
+
+}
+
+export async function deleteUser() {
+    
+}
