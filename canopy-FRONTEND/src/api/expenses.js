5cf13038a34e01f2c53a810aa97ca08b1b8bf761
Ria Bhullar <riabhullar44@gmail.com>
2026-05-14 13:22:10 -0700

    add list of api functions

--- canopy-FRONTEND/src/api/expenses.js
+++ canopy-FRONTEND/src/api/expenses.js
@@ -1,2 +1,45 @@
 const API = import.meta.env.VITE_APP_BASE_URL
 
+export async function getAllExpenses() {
+
+}
+
+export async function getAllQuickExpenses() {
+    
+}
+
+export async function getExpense() {
+
+}
+
+export async function getExpensesInCategory() {
+
+}
+
+export async function createNewExpense() {
+
+}
+
+export async function editExpense() {
+    
+}
+
+export async function deleteExpense() {
+
+}
+
+export async function getCategoryTotals() {
+
+}
+
+export async function compareCategories() {
+
+}
+
+export async function getCategoryTopChanges() {
+
+}
+
+export async function getDateRangeTotal() {
+
+}
