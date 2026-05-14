63518033479ec036bdad882c616de3b1d2da644a
Ria Bhullar <riabhullar44@gmail.com>
2026-05-14 10:17:20 -0700

    add newgoal api call

--- canopy-FRONTEND/src/api/goals.js
+++ canopy-FRONTEND/src/api/goals.js
@@ -1,2 +1,23 @@
 const API = import.meta.env.VITE_APP_BASE_URL
 
+export async function createNewGoal(name, targetAmount) {
+    const res = await fetch(`${API}/goals/new`, {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({
+        name,
+        target_amount: Number(targetAmount),
+        }),
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to create goal");
+    }
+
+    return data;
+}
