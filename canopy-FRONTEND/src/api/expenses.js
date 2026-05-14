fd5477459f34c50b5a04bb3fa8968dc1a64b311b
Ria Bhullar <riabhullar44@gmail.com>
2026-05-14 13:40:29 -0700

    add get expense function

--- canopy-FRONTEND/src/api/expenses.js
+++ canopy-FRONTEND/src/api/expenses.js
@@ -1,7 +1,18 @@
 const API = import.meta.env.VITE_APP_BASE_URL
 
 export async function getAllExpenses() {
+    const res = await fetch(`${API}/list?quickExpense=false`, {
+        method: "GET",
+        credentials: "include",
+    });
 
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to fetch expenses");
+    }
+
+    return data;
 }
 
 export async function getAllQuickExpenses() {
