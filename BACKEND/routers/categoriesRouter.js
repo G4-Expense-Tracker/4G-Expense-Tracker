360d637746c4e0dc1917f3e09a90a94b508b1a48
Ria Bhullar <riabhullar44@gmail.com>
2026-05-15 09:27:55 -0700

    add route skeletons

--- BACKEND/routers/categoriesRouter.js
+++ BACKEND/routers/categoriesRouter.js
@@ -7,3 +7,21 @@ import {
     editCategory,
     deleteCategory
 } from "../db/dal/category.js";
+
+router.post('/new', requireLogin , async (req, res) => {
+
+})
+
+router.get('/:categoryId/view', requireLogin , async (req, res) => {
+
+})
+
+router.post('/:categoryId/edit', requireLogin , async (req, res) => {
+
+})
+
+router.post('/:categoryId/delete', requireLogin , async (req, res) => {
+
+})
+
+export default router;
