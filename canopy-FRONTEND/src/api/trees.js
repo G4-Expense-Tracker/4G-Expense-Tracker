468c52e7da0faf12bb1c977076cfc7a06fcdd388
Ria Bhullar <riabhullar44@gmail.com>
2026-05-14 22:07:41 -0700

    add function skeletons

--- canopy-FRONTEND/src/api/trees.js
+++ canopy-FRONTEND/src/api/trees.js
@@ -1,2 +1,13 @@
 const API = import.meta.env.VITE_APP_BASE_URL
 
+export async function getAllTrees() {
+
+}
+
+export async function getGoalTrees(goalId) {
+
+}
+
+export async function createNewTree(goalId) {
+    
+}
