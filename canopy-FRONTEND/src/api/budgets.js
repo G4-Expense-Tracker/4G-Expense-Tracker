bc25ea86b20475ef0d86488c97916e13c789960c
Ria Bhullar <riabhullar44@gmail.com>
2026-05-15 11:36:26 -0700

    add error checks

--- canopy-FRONTEND/src/api/budgets.js
+++ canopy-FRONTEND/src/api/budgets.js
@@ -2,16 +2,40 @@ const API = import.meta.env.VITE_APP_BASE_URL
 
 export async function createNewCategory() {
 
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to create new category");
+    }
+
+    return data;
 }
 
 export async function getSingleCategory() {
 
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to fetch category");
+    }
+
+    return data;
 }
 
 export async function editSingleCategory() {
 
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to edit category");
+    }
+
+    return data;
 }
 
 export async function deleteSingleCategory() {
-    
+
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to delete category");
+    }
+
+    return data;
 }
