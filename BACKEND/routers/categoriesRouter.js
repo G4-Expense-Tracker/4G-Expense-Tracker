57f2b094efad62c411c3d0e9f3d58ea02a91a86d
Ria Bhullar <riabhullar44@gmail.com>
2026-05-15 09:32:57 -0700

    add try catch statements to routes

--- BACKEND/routers/categoriesRouter.js
+++ BACKEND/routers/categoriesRouter.js
@@ -9,19 +9,39 @@ import {
 } from "../db/dal/category.js";
 
 router.post('/new', requireLogin , async (req, res) => {
+    try {
 
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server failure" });
+    }
 })
 
 router.get('/:categoryId/view', requireLogin , async (req, res) => {
+    try {
 
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server failure" });
+    }
 })
 
 router.post('/:categoryId/edit', requireLogin , async (req, res) => {
+    try {
 
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server failure" });
+    }
 })
 
 router.post('/:categoryId/delete', requireLogin , async (req, res) => {
+    try {
 
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server failure" });
+    }
 })
 
 export default router;
