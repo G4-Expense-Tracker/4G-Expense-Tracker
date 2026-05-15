5abdbb082c5d058f99f582299472d479808ca062
Ria Bhullar <riabhullar44@gmail.com>
2026-05-15 10:31:47 -0700

    add function skeletons

--- canopy-FRONTEND/src/api/budgets.js
+++ canopy-FRONTEND/src/api/budgets.js
@@ -1,2 +1,17 @@
 const API = import.meta.env.VITE_APP_BASE_URL
 
+export async function createNewCategory() {
+
+}
+
+export async function getSingleCategory() {
+
+}
+
+export async function editSingleCategory() {
+
+}
+
+export async function deleteSingleCategory() {
+    
+}
