7c5ebe84a5a1c33d763a628082b28cb9f1a1a7c8
Ria Bhullar <riabhullar44@gmail.com>
2026-05-14 22:10:11 -0700

    add get all trees function

--- canopy-FRONTEND/src/api/trees.js
+++ canopy-FRONTEND/src/api/trees.js
@@ -1,7 +1,18 @@
 const API = import.meta.env.VITE_APP_BASE_URL
 
 export async function getAllTrees() {
+    const res = await fetch(`${API}/trees/list`, {
+        method: "GET",
+        credentials: "include",
+    });
 
+    const data = await res.json();
+
+    if (!res.ok) {
+        throw new Error(data.error || "Failed to fetch trees");
+    }
+
+    return data.userTrees;
 }
 
 export async function getGoalTrees(goalId) {
@@ -9,5 +20,5 @@ export async function getGoalTrees(goalId) {
 }
 
 export async function createNewTree(goalId) {
-    
+
 }
