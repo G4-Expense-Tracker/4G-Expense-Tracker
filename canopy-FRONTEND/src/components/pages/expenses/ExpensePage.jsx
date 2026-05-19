23ae0effb238a857f6ed92632ea705ac9593b11c
Ria Bhullar <riabhullar44@gmail.com>
2026-05-18 22:25:13 -0700

    add import statement

--- canopy-FRONTEND/src/components/pages/expenses/ExpensePage.jsx
+++ canopy-FRONTEND/src/components/pages/expenses/ExpensePage.jsx
@@ -28,6 +28,8 @@ import "@mobiscroll/react/dist/css/mobiscroll.min.css";
 import FooterNav from "../../Footer/FooterNav.jsx";
 import ExpenseHeader from "../../headers/ExpenseHeader.jsx";
 
+import { getAllExpenses } from "../../../api/expenses";
+
 setOptions({
   theme: "ios",
   themeVariant: "light",
