d2b5cb51149b4399f0486d478dc015efa3d22c1d
Ava David <avadavid48@gmail.com>
2026-05-19 13:52:57 -0700

    monthly insight component skeleton created

--- canopy-FRONTEND/src/components/pages/insights_pages/Monthly.jsx
+++ canopy-FRONTEND/src/components/pages/insights_pages/Monthly.jsx
@@ -1,5 +1,60 @@
-function Monthly(){
+import { Box, Typography } from "@mui/material"
+import LinearProgress from '@mui/material/LinearProgress';
 
+function Monthly() {
+
+    const percentAvailable = 12
+
+    return (
+        <Box>
+            {/* progress bar and money left */}
+            <Box>
+                {/* progress bar like goal page */}
+
+                <Box
+                    sx={{
+                        display: "flex"
+                    }}>
+
+                    <Box sx={{ mb: 2 }}>
+                        <LinearProgress
+                            variant="determinate"
+                            value={ percentUsed }
+                        />
+                    </Box>
+
+                    <Box>
+                        <Typography variant="body1" component="p">
+                            Used
+                        </Typography>
+                        <Typography variant="h3" component="p">
+                            {moneyUsed}
+                        </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="body1" component="p">
+                            Remaining
+                        </Typography>
+                        <Typography variant="h3" component="p">
+                            {moneyRemaining}
+                        </Typography>
+                    </Box>
+
+                </Box>
+            </Box>
+
+            {/* percent available */}
+            <Box>
+                <Typography variant="h2" component="h2">
+                    {percentAvailable}
+                </Typography>
+
+                <Typography variant="body1" component="p">
+                    Available
+                </Typography>
+            </Box>
+        </Box>
+    )
 }
 
 export default Monthly
