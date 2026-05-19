b309a756f4cca3400878b80d7d080458e7ecc2d8
Ava David <avadavid48@gmail.com>
2026-05-19 10:55:29 -0700

    top 3 component skeleton made

---
+++ canopy-FRONTEND/src/components/pages/insights_pages/top3/Top3.jsx
@@ -0,0 +1,19 @@
+function Top3(){
+    const top3Changes = [
+        {
+            category_id:
+        }
+
+    ]
+
+    const currentMonthStart = "idk"
+    const prevMonthEnd = "alsoidk"
+
+    return(
+        
+        <
+
+    )
+}
+
+export default Top3
